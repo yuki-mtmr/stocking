8bf658eee9ce632cee7067c06a24166abf74476d
yuki-mtmr <yuki.matsumori.exam@gmail.com>
2020-02-12 17:50:15 +0900

    fix delete

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require turbolinks
+//= require rails-ujs
 //= require jquery
 //= require jquery_ujs
+//= require turbolinks
+//= require bootstrap.min
 //= require_tree .
